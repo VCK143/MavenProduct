f7e4afcea6ff08e8ff9ee3dfc15c75f4141888e8
Chandan <Chandan@Voona_Chandan>
2018-01-05 00:01:11 +0530

    hi

--- MavenProduct/src/main/java/Sample.java
+++ MavenProduct/src/main/java/Sample.java
@@ -1,8 +1,13 @@
 
 public class Sample {
-
+	private int a = 0;
+	
 	public static void main(String[] args) {
+		
 		System.out.println("Hi Chandan");
+		System.out.println(new Sample().a);
+		String b = "10";
+		System.out.println(b);
 	}
 
 }
